48e827770a3acb0a0ffaa6bfc4d43e5d0f84b0ca
Anjeli Verastigue <anjeli.verastigue@tecsup.edu.pe>
2025-04-30 15:49:42 -0500

    Vistas

--- Lab07/blog_platform/src/config/settings.py
+++ Lab07/blog_platform/src/config/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',  # Our blog app
+
 ]
 
 MIDDLEWARE = [
@@ -47,8 +49,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'blog',  # Our blog app
-
 ]
 
 ROOT_URLCONF = 'config.urls'
