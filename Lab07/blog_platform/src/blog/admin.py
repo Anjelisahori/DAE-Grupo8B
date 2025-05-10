875229f6ffaf0a890b4d2678ccb3a0312d9eff5c
AriadnaNolasco <ariadna.nolasco@tecsup.edu.pe>
2025-05-10 17:17:25 -0500

    diseño anterior

--- Lab07/blog_platform/src/blog/admin.py
+++ Lab07/blog_platform/src/blog/admin.py
@@ -1,6 +1,24 @@
 from django.contrib import admin
 from django.utils.html import format_html
+from django.utils.safestring import mark_safe
 from .models import Category, Tag, Post, Comment
+from django.contrib import admin
+from django.contrib.admin import AdminSite
+
+# Personalización del sitio admin
+admin.site.site_header = "Blog Admin"
+admin.site.site_title = "Panel de Administración"
+admin.site.index_title = "Bienvenido al Panel del Blog"
+
+# Personalizamos el sitio de administración
+class CustomAdminSite(AdminSite):
+    class Media:
+        css = {
+            'all': ('admin/css/custom_admin.css',)  # Ruta hacia tu archivo CSS
+        }
+
+# Configuración predeterminada del sitio admin
+admin.site.__class__ = CustomAdminSite
 
 
 @admin.register(Category)
@@ -35,7 +53,7 @@ class CommentInline(admin.TabularInline):
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'category', 'status', 'created_at', 'comment_count', 'display_image')
+    list_display = ('title', 'author', 'category', 'colored_status', 'created_at', 'comment_count', 'display_image')
     list_filter = ('status', 'category', 'created_at', 'author')
     search_fields = ('title', 'content')
     prepopulated_fields = {'slug': ('title',)}
@@ -66,6 +84,23 @@ class PostAdmin(admin.ModelAdmin):
         return obj.comments.count()
     comment_count.short_description = 'Comments'
 
+    def colored_status(self, obj):
+        if obj.status == "published":
+            color = "green"
+            icon = "🟢"
+        elif obj.status == "draft":
+            color = "orange"
+            icon = "🟠"
+        else:
+            color = "gray"
+            icon = "⚪"
+        return format_html(
+            '<span style="font-weight: bold; color: {}; font-size: 14px;">{} {}</span>',
+            color,
+            icon,
+            obj.get_status_display()
+        )
+    colored_status.short_description = 'Status'
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
@@ -73,6 +108,7 @@ class CommentAdmin(admin.ModelAdmin):
     list_filter = ('is_approved', 'created_at')
     search_fields = ('content', 'author__username', 'post__title')
     date_hierarchy = 'created_at'
+    list_editable = ('is_approved',)
     actions = ['approve_comments']
     
     def approve_comments(self, request, queryset):
